592da6e51ed08abc7f8f0f63f05dda5a00d9ae56
Snikimonkd <cepminkis132@gmail.com>
2020-11-10 22:50:21 +0300

    create headders

---
+++ inc/create_new_page.hpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+
+enum page_error { SUCCESS, FAILURE };
+
+enum page_error create_new_page(const std::string &new_page_url){};
+
+enum page_error create_new_dir(const std::string &dir_name){};
