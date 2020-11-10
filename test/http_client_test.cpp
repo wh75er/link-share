a195e57185ddacd3eab04ea52eb2a81dd83742c2
Snikimonkd <cepminkis132@gmail.com>
2020-11-10 23:58:10 +0300

    changed cmake files

--- test/http_client_test.cpp
+++ test/http_client_test.cpp
@@ -1,8 +1,9 @@
 #include "gtest/gtest.h"
 #include <string>
 
-extern "C" {
 #include "http_client.hpp"
-}
 
-TEST(http_client_test, constructor_test) { EXPECT_EQ(2, (1 + 1)); }
+TEST(http_client_test, create_request_test) {
+    foo();
+    EXPECT_EQ(2, (1 + 1));
+}
