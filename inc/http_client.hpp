1a1de766a29f2541f7d2fcf52c42b3726c0ce174
Snikimonkd <cepminkis132@gmail.com>
2020-11-11 02:04:49 +0300

    added tests got client

--- inc/http_client.hpp
+++ inc/http_client.hpp
@@ -12,16 +12,16 @@ enum client_exit_status {
     SUCCESS,
     CONNECTION_ERROR,
     BAD_REQUEST_ERROR,
-    SOCKET_CREATION_ERROR
+    SOCKET_CREATION_ERROR,
     /* some other errors */
 };
 
 class http_client {
 public:
     http_client();
-    enum client_exit_status recieve(std::string &path_to_response);
+    enum client_exit_status recieve(std::string *path_to_response);
     enum client_exit_status send(const std::string &request);
-    const std::string &create_request(const std::string &url);
+    std::string create_request(const std::string &url);
 
 private:
     int socket_fd;
@@ -29,5 +29,3 @@ private:
     SSL *ssl;
     sockaddr_in socket_adress;
 };
-
-void foo();
