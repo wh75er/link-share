7a691cb28c547aa8431cfbe1a10a1221bdddb57a
Snikimonkd <cepminkis132@gmail.com>
2020-11-11 02:47:20 +0300

    bad code

--- inc/parser.hpp
+++ inc/parser.hpp
@@ -3,27 +3,26 @@
 #include <string.h>
 #include <vector>
 
-enum parser_exit_status { SUCCESS, FAILURE };
+enum parser_exit_status { SUCCESS_PARSE, FAILURE_PARSE };
 
 class parser {
     std::vector<std::string> sources;
     std::string url;
 
 public:
-    virtual ~parser() = 0;
     virtual enum parser_exit_status parse(const std::string &path_to_file) = 0;
-    virtual std::string &get_src_url_from_string(const std::string &str) = 0;
+    virtual std::string get_src_url_from_string(const std::string &str) = 0;
 };
 
 class html_parser : public parser {
 public:
     enum parser_exit_status parse(const std::string &path_to_file);
-    std::string &get_src_url_from_string(const std::string &str);
-    const std::string &type_of_src(const std::string &str);
+    std::string get_src_url_from_string(const std::string &str);
+    bool is_css(const std::string &str);
 };
 
 class css_parser : public parser {
 public:
     enum parser_exit_status parse(const std::string &path_to_file);
-    std::string &get_src_url_from_string(const std::string &str);
+    std::string get_src_url_from_string(const std::string &str);
 };
