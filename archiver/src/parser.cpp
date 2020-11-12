a78c3cbdf96e302f6162955e67fe6277a15dee0d
Snikimonkd <cepminkis132@gmail.com>
2020-11-12 15:01:38 +0300

    parser tests

--- archiver/src/parser.cpp
+++ archiver/src/parser.cpp
@@ -1,19 +1,19 @@
-#include "../inc/parser.hpp"
+#include "parser.hpp"
 
 parser_exit_status html_parser::parse(const std::string &path_to_file) {
     return SUCCESS_PARSE;
 }
 
-std::string html_parser::get_src_url_from_string(const std::string &str) {
-    return "some string";
+std::string *html_parser::get_src_url_from_string(const std::string &str) {
+    return nullptr;
 }
 
 bool html_parser::is_css(const std::string &str) { return false; }
 
 parser_exit_status css_parser::parse(const std::string &path_to_file) {
-    return SUCCESS_PARSE;
+    return FAILURE_PARSE;
 }
 
-std::string css_parser::get_src_url_from_string(const std::string &str) {
-    return "some string";
+std::string *css_parser::get_src_url_from_string(const std::string &str) {
+    return nullptr;
 }
