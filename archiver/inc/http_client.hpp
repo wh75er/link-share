d100df4eaaf9a74f619c283fc84d0e15e07d7bee
Snikimonkd <cepminkis132@gmail.com>
2020-11-20 22:51:02 +0300

    http_client constructor, connect, request&recieve structures

--- archiver/inc/http_client.hpp
+++ archiver/inc/http_client.hpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 #include <arpa/inet.h>
+#include <netdb.h>
 #include <netinet/in.h>
 #include <openssl/err.h>
 #include <openssl/ssl.h>
@@ -10,24 +11,44 @@
 
 enum client_exit_status {
     SUCCESS,
-    CONNECTION_ERROR,
+    CONNECTION_FAILURE,
+    CONNECTION_SUCCESS,
     BAD_REQUEST_ERROR,
     SOCKET_CREATION_ERROR,
+    RESOLV_ERROR
     /* some other errors */
+};
+
+struct http_request {
+    http_request() : query("\0"), host("\0"){};
+    http_request(const std::string &url);
+    std::string query;
+    std::string host;
+};
+
+struct http_response {
+    http_response() : query("\0"), html_body("\0"){};
+    http_response(const std::string &response);
+    std::string query;
+    std::string html_body;
 };
 
 class http_client {
 public:
-    http_client();
+    http_client(const std::string &url);
     http_client(const http_client &other) = delete;
     http_client &operator=(const http_client &other) = delete;
     ~http_client();
 
-    enum client_exit_status connect(const std::string &url);
+    enum client_exit_status connect();
     enum client_exit_status recieve(std::string *path_to_response);
     enum client_exit_status send(const std::string &request);
     std::string create_request(const std::string &url);
 
 private:
     int socket_fd;
+    http_request request;
+    http_response response;
+    struct sockaddr_in addr;
+    struct sockaddr_in resolve(const std::string &url);
 };
--- archiver/test/http_client_test.cpp
+++ archiver/test/http_client_test.cpp
@@ -3,7 +3,8 @@
 #include "http_client.hpp"
 
 TEST(http_client, create_request_test) {
-    http_client new_client;
+    http_client new_client("www.any.com");
+    new_client.connect();
     std::string request =
         new_client.create_request("https://www.any.com/something");
     ASSERT_STREQ(
@@ -12,19 +13,22 @@ TEST(http_client, create_request_test) {
 }
 
 TEST(http_client, send_test_1) {
-    http_client new_client;
+    http_client new_client("ASD");
+    new_client.connect();
     ASSERT_EQ(new_client.send("asd"), BAD_REQUEST_ERROR);
 }
 
 TEST(http_client, send_test_2) {
-    http_client new_client;
+    http_client new_client("www.example.com");
+    new_client.connect();
     ASSERT_EQ(new_client.send("GET https://www.example.com HTTP/1.1\r\nHost: "
                               "www.example.com\r\n"),
               SUCCESS);
 }
 
 TEST(http_client, recieve_test) {
-    http_client new_client;
+    http_client new_client("www.example.com");
+    new_client.connect();
     ASSERT_EQ(new_client.send("GET https://www.example.com HTTP/1.1\r\nHost: "
                               "www.example.com\r\n"),
               SUCCESS);
@@ -34,6 +38,6 @@ TEST(http_client, recieve_test) {
 }
 
 TEST(http_client, connect_test) {
-    http_client new_client;
-    ASSERT_EQ(new_client.connect("https://www.example.com"), SUCCESS);
+    http_client new_client("www.example.com");
+    ASSERT_EQ(new_client.connect(), SUCCESS);
 }
