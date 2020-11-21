7667e6f9e0506c7b5037b4b19bba56e9ed9b137e
Snikimonkd <cepminkis132@gmail.com>
2020-11-21 03:34:58 +0300

    connect,recieve,send no error checks

--- archiver/src/http_client.cpp
+++ archiver/src/http_client.cpp
@@ -10,12 +10,57 @@ http_request::http_request(const std::string &url) {
     query += "\r\n";
 }
 
+http_response::http_response(const std::string &response) {
+    std::string::size_type pos = response.find("<!doctype html>");
+    query = response.substr(0, pos);
+    html_body = response.substr(pos, response.length() - pos);
+}
+
 http_client::http_client(const std::string &url) {
     socket_fd = socket(AF_INET, SOCK_STREAM, 0);
     /* надо обработать ошибку */
+    if (socket_fd == -1) {
+        std::cout << "failconnect1";
+    }
     request = http_request(url);
     memset(&addr, 0, sizeof(addr));
     addr = resolve(url);
+
+    int connected =
+        ::connect(socket_fd, (struct sockaddr *)&addr, sizeof(addr));
+    if (connected == -1) {
+        std::cout << "asd";
+        std::cout << connected;
+        std::cout << errno;
+    }
+
+    SSL_library_init();
+    SSLeay_add_ssl_algorithms();
+    const SSL_METHOD *meth = TLS_client_method();
+    SSL_CTX *ctx = SSL_CTX_new(meth);
+    ssl = SSL_new(ctx);
+    /*error*/
+    if (!ssl) {
+        std::cout << "failconnect2";
+    }
+
+    int sock = SSL_get_fd(ssl);
+    /*error*/
+    SSL_set_fd(ssl, socket_fd);
+
+    int err = SSL_connect(ssl);
+    if (err <= 0) {
+        std::cout << "huy" << err << SSL_get_error(ssl, err);
+    }
+}
+
+http_client::~http_client() { socket_fd = 0; }
+
+enum client_exit_status http_client::connect() {
+
+    /*error*/
+
+    return CONNECTION_SUCCESS;
 }
 
 struct sockaddr_in http_client::resolve(const std::string &url) {
@@ -43,30 +88,27 @@ struct sockaddr_in http_client::resolve(const std::string &url) {
     return addr;
 }
 
-http_client::~http_client() {
-    int a = 0;
-    a++;
-}
-
 std::string http_client::create_request(const std::string &url) {
     std::string request = "some string";
     return request;
 }
 
-enum client_exit_status http_client::connect() {
-    int connected =
-        ::connect(socket_fd, (struct sockaddr *)&addr, sizeof(addr));
-    if (connected == -1) {
-        return CONNECTION_FAILURE;
-    }
-
-    return CONNECTION_SUCCESS;
-}
-
-enum client_exit_status http_client::recieve(std::string *path_to_response) {
+enum client_exit_status http_client::recieve(std::string *response) {
+    int len = 100;
+    char buf[10000];
+    do {
+        len = SSL_read(ssl, buf, 100);
+        buf[len] = 0;
+        printf("%s\n", buf);
+    } while (len > 0);
     return SUCCESS;
 }
 
-enum client_exit_status http_client::send(const std::string &request) {
+enum client_exit_status http_client::send() {
+    int len =
+        SSL_write(ssl, request.query.c_str(), strlen(request.query.c_str()));
+    if (len < 0) {
+        std::cout << "ne otpravilos";
+    }
     return CONNECTION_FAILURE;
 }
