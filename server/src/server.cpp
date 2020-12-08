70ea560e3544bc77d4ec2d968ab2d2937eadef92
wh75er <wh75er@gmail.com>
2020-12-08 23:27:07 +0300

    implement constructor and change class members

--- server/src/server.cpp
+++ server/src/server.cpp
@@ -1,11 +1,29 @@
 #include "server.hpp"
 
-TCPserver::TCPserver(std::string address, int port)
-  : acceptor_(ioc_),
-    address_(address), 
-    port_(port)
+TCPserver::TCPserver(std::string address, std::string port):
+  address_(address), 
+  port_(port)
 {
   // Create non-blocking socket and bind it
+  socket_ = TcpSocket::Builder()
+    .address(address)
+    .port(port)
+    .non_blocking()
+    .build();
+
+  try {
+    socket_.create();
+  } 
+  catch (const BaseException& except) {
+    throw ServerException(except.error);
+  }
+
+  try {
+    socket_.bind_();
+  }
+  catch (const BaseException& except) {
+    throw ServerException(except.error);
+  }
 }
 
 void TCPserver::accept(std::shared_ptr<Connection> connection) {
@@ -14,12 +32,16 @@ void TCPserver::accept(std::shared_ptr<Connection> connection) {
 
 void TCPserver::listen() {
   // listen for connections
+  try {
+    socket_.listen_();
+  }
+  catch (const BaseException& except) {
+    throw ServerException(except.error);
+  }
   
   // If connection available, accept it(create Connection)
   
   // Run Connection
 
   // Continue to listen for other connections
-
-  return;
 }
