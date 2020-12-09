dcb69adde6d23bfb3235d651338a88ae4d45539c
wh75er <wh75er@gmail.com>
2020-12-09 22:44:24 +0300

    add debug info + features iterating condition

--- server/include/connection.hpp
+++ server/include/connection.hpp
@@ -11,7 +11,7 @@
 class Connection : public std::enable_shared_from_this<Connection> {
 public:
   explicit Connection(const BaseTcpSocket& socket);
-  ~Connection() {}
+  ~Connection() {std::cout << "Connection destructor" << std::endl;};
 
   std::string read();
   void write(std::string data);
