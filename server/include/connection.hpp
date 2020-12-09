2553fd44f8cf37993311229593f3cf1706945c00
wh75er <wh75er@gmail.com>
2020-12-09 04:31:48 +0300

    remove boost from connection class

--- server/include/connection.hpp
+++ server/include/connection.hpp
@@ -1,24 +1,22 @@
 #pragma once
 
+#include <iostream>
+
 #include <string.h>
 #include <memory>
 
-#define BOOST_ASIO_HAS_MOVE 1
-
-#include <boost/asio.hpp>
-#include <boost/asio/ip/tcp.hpp>
-
 #include "requestHandler.hpp"
+#include "socket.hpp"
 
 class Connection : public std::enable_shared_from_this<Connection> {
 public:
-  explicit Connection(boost::asio::ip::tcp::socket socket);
+  explicit Connection(BaseTcpSocket socket);
   ~Connection();
 
   std::string read();
   void write(std::string data);
 
 private:
-  boost::asio::ip::tcp::socket socket_;
+  BaseTcpSocket socket_;
   std::unique_ptr<RequestHandler> rh;
 };
