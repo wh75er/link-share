d18b583864a3f6649d6c84b98f27bf74ac69b659
Snikimonkd <cepminkis132@gmail.com>
2020-12-16 23:58:22 +0300

    delete

--- archiver/test/https_socket_test.cpp
+++ archiver/test/https_socket_test.cpp
@@ -44,5 +44,7 @@ TEST(https_socket, send_recv_image_test) {
     file.read(test_file, new_response.contentLength);
     EXPECT_STREQ(test_file, new_response.body);
 
+    delete[] test_file;
+
     file.close();
 }
