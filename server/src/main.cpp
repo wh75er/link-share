a8bbad513f7205e863055129ddff69125b20bca1
wh75er <wh75er@gmail.com>
2020-12-20 17:18:30 +0300

    Add json parser and uuid generator

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -1,8 +1,56 @@
 #include "server.hpp"
 #include "errors.hpp"
-#include "socket.hpp"
+#include <sstream>
 #include <string>
 #include <iostream>
+#include <boost/uuid/uuid.hpp>
+#include <boost/uuid/uuid_generators.hpp>
+#include <boost/uuid/uuid_io.hpp>
+#include <boost/property_tree/json_parser.hpp>
+
+#include "pgconnection.hpp"
+
+class Uuid {
+public:
+  void generate() {
+    u = gen();
+  }
+
+  std::string to_string(){
+    if (u.is_nil()) {
+      return "";
+    }
+    return boost::uuids::to_string(u);
+  }
+
+private:
+  boost::uuids::uuid u;
+  boost::uuids::random_generator gen;
+};
+
+class JsonParser {
+public:
+  explicit JsonParser(std::string& json_str) {
+    std::stringstream ss;
+
+    ss << json_str;
+
+    boost::property_tree::read_json(ss, pt);
+  }
+
+  template<typename T>
+  bool get_value(std::string& key, T& value) {
+    if (boost::optional<T> v = pt.get_optional<T>(key)) {
+      value = *v;
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+private:
+  boost::property_tree::ptree pt;
+};
 
 int main() {
   std::string address = "127.0.0.1";
